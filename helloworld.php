2325180e60d60c4d1aaca16c3ead830deee1c379
Sarimoko <TravisReames@gmail.com>
2016-09-29 15:29:29 -0700

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -1,5 +1,8 @@
 <?php
+$output_helloworld = file_get_contents('helloworld.txt');
+$bot_input = $_GET['botinput'];
 
-echo $_GET['botinput'];
-
+if(empty($bot_input)) echo $output_helloworld;
+elseif($bot_input == "helloworld") echo $output_helloworld;
+else echo $bot_input;
 ?>
